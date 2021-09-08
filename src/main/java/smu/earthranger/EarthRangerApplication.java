8d3ac49dbbedd7250ae55718008225359a6f2e38
semi_cloud <sksmsrkdtpal@naver.com>
2021-09-08 17:56:15 +0900

    init

---
+++ src/main/java/smu/earthranger/EarthRangerApplication.java
@@ -0,0 +1,13 @@
+package smu.earthranger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthRangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthRangerApplication.class, args);
+	}
+
+}
