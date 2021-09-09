d8e91e44634be90e8396e3e21a07b8930c48006b
younghyun-na <younghyunna@naver.com>
2021-09-09 14:48:38 +0900

    Update User.java
    feat: user entity

--- src/main/java/smu/earthranger/domain/User.java
+++ src/main/java/smu/earthranger/domain/User.java
@@ -3,6 +3,7 @@ package smu.earthranger.domain;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,9 +13,16 @@ import javax.persistence.Id;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue
     @Column(name = "user_id")
     private Long id;
+
+    private String password;
+    private String userName;
+    private String email;
+    private Integer tree_id;
+    private Integer following_count;
 }
