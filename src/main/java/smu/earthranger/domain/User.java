97bef3cf78608f89d51588acfb29c5030f387ac7
younghyun-na <younghyunna@naver.com>
2021-09-09 19:10:29 +0900

    feat: User entity 연관관계 매핑 수정

--- src/main/java/smu/earthranger/domain/User.java
+++ src/main/java/smu/earthranger/domain/User.java
@@ -5,17 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Setter
-public class User {
-    
+public class User extends BaseTimeEntity{
+
     @Id
     @GeneratedValue
     @Column(name = "user_id")
@@ -24,6 +23,28 @@ public class User {
     private String password;
     private String userName;
     private String email;
-    private Integer tree_id;
+
+    private Integer tree_level;
+    private Integer follower_count;
     private Integer following_count;
+
+    @OneToMany(mappedBy = "user")
+    private List<Carbon> carbon = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Tree> tree = new ArrayList<>();
+
+    @OneToMany(mappedBy = "from_user_id")
+    private List<Follow> followers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "to_user_id")
+    private List<Follow> followings = new ArrayList<>();
+
+
+
+
+
+
+
+
 }
