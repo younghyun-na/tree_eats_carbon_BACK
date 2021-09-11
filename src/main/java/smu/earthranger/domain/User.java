956b7de4c8622b796e2847f0642e5209259cdba2
younghyun-na <younghyunna@naver.com>
2021-09-11 17:43:59 +0900

    feat: user entity 추가

--- src/main/java/smu/earthranger/domain/User.java
+++ src/main/java/smu/earthranger/domain/User.java
@@ -3,7 +3,6 @@ package smu.earthranger.domain;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -12,7 +11,6 @@ import java.util.List;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Setter
 public class User extends BaseTimeEntity{
 
     @Id
@@ -21,9 +19,10 @@ public class User extends BaseTimeEntity{
     private Long id;
 
     private String password;
-    private String userName;
+    private String user_name;
     private String email;
 
+    private Integer rank;
     private Integer tree_level;
     private Integer follower_count;
     private Integer following_count;
@@ -31,20 +30,9 @@ public class User extends BaseTimeEntity{
     @OneToMany(mappedBy = "user")
     private List<Carbon> carbon = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user")
-    private List<Tree> tree = new ArrayList<>();
-
-    @OneToMany(mappedBy = "from_user_id")
+    @OneToMany(mappedBy = "from_user")
     private List<Follow> followers = new ArrayList<>();
 
-    @OneToMany(mappedBy = "to_user_id")
+    @OneToMany(mappedBy = "to_user")
     private List<Follow> followings = new ArrayList<>();
-
-
-
-
-
-
-
-
 }
