6121dfb14671b851192fe31b1c605b4010e20fa2
jimin3263 <dkdldsla0000@naver.com>
2021-09-20 01:15:41 +0900

    feat: rank - 전체 회원, 팔로우 회원 순위 추가

--- src/main/java/smu/earthranger/controller/RankController.java
+++ src/main/java/smu/earthranger/controller/RankController.java
@@ -1,6 +1,7 @@
 package smu.earthranger.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import smu.earthranger.dto.user.MemberRankingResponseDto;
 import smu.earthranger.service.RankService;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,13 +22,11 @@ public class RankController {
     private final RankService rankService;
     private Long userId = 1L; //임시
 
-
     @GetMapping
-    public ResponseEntity<?> getRank(@RequestParam(defaultValue = "all") String user,
+    public ResponseEntity<?> getRank(@RequestParam String user,
                                      @PageableDefault(size = 20, sort = "totalReduction", direction = Sort.Direction.DESC) Pageable pageable){
-        List<MemberRankingResponseDto> dto = rankService.getByPermission(userId, user, pageable);
+        Page<MemberRankingResponseDto> dto = rankService.getByPermission(userId, user, pageable);
         return ResponseEntity.ok(dto);
     }
-
 
 }
